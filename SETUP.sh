945ee3a92432071f541b8a1572272d54eeda32eb
vince <siuv@lnxsrv04.seas.ucla.edu>
2016-02-03 09:55:55 -0800

    Updated SETUP.sh, so now it adds a new .sh file, but it doesn't do anything shnazzy if there's already a .vimrc already in home

--- SETUP.sh
+++ SETUP.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 #Perhaps I should make a check for .vimrc presence already, and throw out a message?
-ln -s ./.vimrc ~/.vimrc
+
+FILE="~/.vimrc"
+FILEPATH=`pwd`
+FILEPATH="$FILEPATH/.vimrc"
+if [ -f $FILE ];
+then
+  echo ".vimrc already present, delete first"
+else
+  ln -s $FILEPATH ~/.vimrc
+fi
+  #perhaps add functinoality to delete if I want to?
+#ln -s ./.vimrc ~/.vimrc
