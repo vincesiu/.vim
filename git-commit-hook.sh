fad70663d3713e18d3f737aa81d1c5c2bb8be4b6
Vincent Siu <vince.siu4@gmail.com>
2016-07-29 09:41:28 -0700

    Update git commit hook script
    Change it such that the rebase will only be applied if the current
    branch is master, and will also re checkout master so that I'm not
    flying between branches haha.

--- git-commit-hook.sh
+++ git-commit-hook.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-git rebase master arch
+#only execute this if this is master
+branchname=$(git symbolic-ref --short HEAD)
+
+if [[ $branchname = "master" ]]; then
+  git rebase master arch
+  git checkout master
+fi
