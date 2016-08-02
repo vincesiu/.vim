c2db1789520692c0080c349fa486b58948ab2d6e
Vincent Siu <vince.siu4@gmail.com>
2016-08-01 17:59:49 -0700

    Fix up installation
    Messed up the order of linking, now the correct order happens

--- git-commit-hook.sh
+++ git-commit-hook.sh
@@ -3,7 +3,11 @@
 #only execute this if this is master
 branchname=$(git symbolic-ref --short HEAD)
 
+echo "Running custom post-commit hook..."
+
 if [[ $branchname = "master" ]]; then
   git rebase master arch
   git checkout master
 fi
+
+echo "Done"
